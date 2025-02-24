b12172304485c41e4ca8ea38b8a69281853853ac
DEVANSARAVANAN <devan.saravanan1502@gmail.com>
2025-02-24 16:53:47 +0530

    Chart1  Modified

--- app/components/chart1.py
+++ app/components/chart1.py
@@ -4,13 +4,8 @@ import streamlit as st
 def display_accuracy_chart(final_report):
     print("++"*100,"Line plot","++"*100,)
 
-    iterations_list = [0]
     f1_scores_list = [0]
     for iteration_key, iteration_data in final_report.items():  
-        if iteration_data['iteration']==0:
-            pass
-        else:
-            iterations_list.append(iteration_data['iteration'])
         f1_scores_list.append(iteration_data['accuracy_score']['macro-avg-f1_score']*100)
     iterations_list=[i for i in range(len(f1_scores_list))]
 
